d520f4823c06f8a8f00f6c80f42e8a07fbdbb3a1
Hammad Khan <103638560+hammad63100@users.noreply.github.com>
2025-01-22 15:57:34 +0500

    `Added tests for Token contract deployment and basic functionality`

--- test/token.js
+++ test/token.js
@@ -18,7 +18,7 @@ import { expect } from "chai";
 
 //     it('should transfer token between accounts ', async function () {
 //         const [owner,addr1,addr2] = await ethers.getSigners();
-      
+
 
 //         const Token = await ethers.getContractFactory('Token'); // instance contract
 
@@ -40,7 +40,7 @@ import { expect } from "chai";
 
 
 
-describe('Token contract', function() {
+describe('Token contract', function () {
 
     let Token;
     let hardhatToken;
@@ -49,10 +49,38 @@ describe('Token contract', function() {
     let addr2;
     let addrs;
 
-    beforeEach(async function(){
+    beforeEach(async function () {
         Token = await ethers.getContractFactory('Token'); // instance contract 
 
         [owner, addr1, addr2, ...addrs] = await ethers.getSigners(); // get signers
+
+        hardhatToken = await Token.deploy(); // deploy contract 
     })
-    
+
+
+    describe("Deployment", function () {
+        it('Should  set the right owner', async function () {
+            expect(await hardhatToken.owner()).to.equal(owner.address);
+        }
+        );
+
+        it('Should assign the total supply of tokens to the owner', async function () {
+            const ownerBalance = await hardhatToken.balanceOf(owner.address);
+            expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+        });
+
+        describe("Transactions", function () {
+            it('Should transfer token correctly', async function () {
+                
+                await hardhatToken.transfer(addr1.address, 5);
+                const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+
+
+
+
+
+
+            });
+        });
+    });
 });
