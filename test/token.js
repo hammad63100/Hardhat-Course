99e4fd353e95e9a815498f33dd994203c7a9ea07
Hammad Khan <103638560+hammad63100@users.noreply.github.com>
2025-01-23 10:24:28 +0500

    Add chai-as-promised dependency and update token tests for better assertions

--- test/token.js
+++ test/token.js
@@ -1,44 +1,8 @@
+import * as chai from "chai";
 import { expect } from "chai";
+import chaiAsPromised from "chai-as-promised";
 
-// describe('Token contract', function() {
-//     it('Deployment should assign the total supply of tokens to the owner ', async function () {
-//         const [owner] = await ethers.getSigners();
-//         console.log("Signers Object: ", owner);
-
-//         const Token = await ethers.getContractFactory('Token'); // instance contract
-
-//         const hardhatToken = await Token.deploy(); // deploy contract
-
-//         const ownerBalance = await hardhatToken.balanceOf(owner.address);
-//         console.log("Owner Address: ", owner.address); // ownerBalance = 100000
-
-//         expect((await hardhatToken.totalSupply()).eq(ownerBalance)).to.be.true;  //totalSupply = 100000
-//     });
-
-
-//     it('should transfer token between accounts ', async function () {
-//         const [owner,addr1,addr2] = await ethers.getSigners();
-
-
-//         const Token = await ethers.getContractFactory('Token'); // instance contract
-
-//         const hardhatToken = await Token.deploy(); // deploy contract
-
-//         // transfer 10 token from owner to addr1
-//         await hardhatToken.transfer(addr1.address,10);
-
-//         expect((await hardhatToken.balanceOf(addr1.address)).toNumber()).to.equal(10);
-
-//         // transfer 5 token from addr1 to addr2
-//         await hardhatToken.connect(addr1).transfer(addr2.address,5);
-
-//         expect((await hardhatToken.balanceOf(addr2.address)).toNumber()).to.equal(5);
-
-
-//     });
-// });
-
-
+chai.use(chaiAsPromised);
 
 describe('Token contract', function () {
 
@@ -61,26 +25,39 @@ describe('Token contract', function () {
     describe("Deployment", function () {
         it('Should  set the right owner', async function () {
             expect(await hardhatToken.owner()).to.equal(owner.address);
-        }
-        );
+        });
 
         it('Should assign the total supply of tokens to the owner', async function () {
             const ownerBalance = await hardhatToken.balanceOf(owner.address);
-            expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+            expect((await hardhatToken.totalSupply()).eq(ownerBalance)).to.be.true;
         });
 
         describe("Transactions", function () {
             it('Should transfer token correctly', async function () {
-                
                 await hardhatToken.transfer(addr1.address, 5);
                 const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+                expect(addr1Balance.toNumber()).to.equal(5);
 
-
-
-
-
-
+                await hardhatToken.connect(addr1).transfer(addr2.address, 5);
+                const addr2Balance = await hardhatToken.balanceOf(addr2.address);
+                expect(addr2Balance.toNumber()).to.equal(5);
             });
+
+            it("Should fail if sender doesn't have enough tokens", async function () {
+                const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
+                await expect(
+                    hardhatToken.connect(addr1).transfer(owner.address, 1)
+                ).to.be.rejectedWith("Not enough tokens");
+
+                expect((await hardhatToken.balanceOf(owner.address)).eq(initialOwnerBalance)).to.be.true;
+            });
+
+
+            it('Should update balance after transfers', async () => {
+                const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
+                 
+            });
+            
         });
     });
 });
