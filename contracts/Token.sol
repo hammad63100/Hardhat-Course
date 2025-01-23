145827424c500405dee3fc9156d8df9c1ced1734
Hammad Khan <103638560+hammad63100@users.noreply.github.com>
2025-01-23 10:44:22 +0500

    Add console logging to transfer function in Token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,6 +3,7 @@ pragma solidity  ^0.8.0;
 
 import "hardhat/console.sol";
 
+
 contract Token {
     string public name = "My Hardhat Token";
     string public symbol = "MHT";
@@ -19,6 +20,7 @@ contract Token {
     }
 
     function transfer(address to,uint amount) external{
+        console.log("Sender balance is %s tokens", balances[msg.sender]);
         require(balances[msg.sender] >= amount, "Not enough tokens");
         balances[msg.sender] -= amount;
         balances[to] += amount;
